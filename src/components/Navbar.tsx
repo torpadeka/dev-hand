aea2f9eb45b0385fc1bb6f4c0846e03fdf12ee8b
Darren Kent <darrenkent88@gmail.com>
2025-02-05 14:14:17 +0700

    Continued progress on Navbar

--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -1,14 +1,14 @@
 import Image from "next/image";
-import { Avatar, AvatarFallback, AvatarImage } from "./ui/avatar";
+import NavbarAvatar from "./NavbarAvatar";
 
 export default function Navbar() {
     return (
-        <div className="w-full h-20 flex items-center justify-between px-4 border-b-2 border-gray-800 bg-black">
+        <div className="w-full h-16 flex items-center justify-between px-6 border-b-2 border-gray-800">
             <div className="flex items-center justify-center gap-2">
                 <Image
                     src="./dev-hand.svg"
-                    width={48}
-                    height={48}
+                    width={40}
+                    height={40}
                     alt="#"
                 ></Image>
                 <div className="font-bold text-2xl text-[#3488fa]">
@@ -16,12 +16,7 @@ export default function Navbar() {
                 </div>
             </div>
             <div>
-                <Avatar>
-                    <AvatarImage src="./dev-hand.svg"></AvatarImage>
-                    <AvatarFallback>
-                        <Image src="./dev-hand.svg" alt="DH"></Image>
-                    </AvatarFallback>
-                </Avatar>
+                <NavbarAvatar></NavbarAvatar>
             </div>
         </div>
     );

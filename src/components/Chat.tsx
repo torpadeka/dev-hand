8fbe46e94c2c83c24f985d72b498e22099d90869
Darren Kent <darrenkent88@gmail.com>
2025-02-06 13:41:51 +0700

    Removed socketio and its package

--- src/components/Chat.tsx
+++ src/components/Chat.tsx
@@ -1,48 +1,48 @@
-import React, { useEffect, useRef, useState } from "react";
-import { io,Socket } from "socket.io-client";
+// import React, { useEffect, useRef, useState } from "react";
+// import { io,Socket } from "socket.io-client";
 
-const Chat: React.FC = () => {
+// const Chat: React.FC = () => {
 
-    const ws = useRef<Socket | null>(null); 
-    const [message,setMessages] = useState<string[]>([]);
-    const [peerID] = useState<string>(Math.random().toString(36).substring(2,15))
+//     const ws = useRef<Socket | null>(null); 
+//     const [message,setMessages] = useState<string[]>([]);
+//     const [peerID] = useState<string>(Math.random().toString(36).substring(2,15))
         
 
-    useEffect(() => {
+//     useEffect(() => {
 
-        ws.current = io('http://localhost/3000');
+//         ws.current = io('http://localhost/3000');
 
-        ws.current.on('connect',() => {
-            console.log("socket open")
-        })
+//         ws.current.on('connect',() => {
+//             console.log("socket open")
+//         })
 
-        ws.current.on('disconnect',() => {
-            console.log("socket close")
-        })
+//         ws.current.on('disconnect',() => {
+//             console.log("socket close")
+//         })
 
-        ws.current.on('message',(msg:string) => {
-            setMessages((prevMessage) => [...prevMessage,msg])
-        })
+//         ws.current.on('message',(msg:string) => {
+//             setMessages((prevMessage) => [...prevMessage,msg])
+//         })
 
-        return () => {
-            ws.current?.off('connect');
-            ws.current?.off('disconnect');
-            ws.current?.off('close');
+//         return () => {
+//             ws.current?.off('connect');
+//             ws.current?.off('disconnect');
+//             ws.current?.off('close');
 
-            ws.current?.disconnect()
-        }   
+//             ws.current?.disconnect()
+//         }   
 
-    },[])
+//     },[])
 
-    const sendMessage = (msg : string) => {
-        if(ws.current){
-            ws.current.emit('message',msg);
-        }
-    }
+//     const sendMessage = (msg : string) => {
+//         if(ws.current){
+//             ws.current.emit('message',msg);
+//         }
+//     }
     
-    return <>
+//     return <>
 
-    </>
-}
+//     </>
+// }
 
-export default Chat
+// export default Chat

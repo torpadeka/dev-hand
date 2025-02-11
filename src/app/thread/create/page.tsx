b70eb9b99c7325a00cf4d4d8bff39d5cfb6c9702
F4E12 <76392444+F4E12@users.noreply.github.com>
2025-02-11 10:03:33 +0700

    fix rounded and add create page

---
+++ src/app/thread/create/page.tsx
@@ -0,0 +1,19 @@
+"use client";
+
+import Navbar from "@/components/Navbar";
+import React, { useState } from "react";
+
+export default function Chatbot() {
+  return (
+    <div className="">
+      <Navbar />
+
+      <div className="">
+        <div className="text-xl font-bold">Title</div>
+        <div className="">
+          <input type="text" />
+        </div>
+      </div>
+    </div>
+  );
+}
